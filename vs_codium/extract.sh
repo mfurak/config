51be47c9a25c09a9b83f1ce1fbdf4f56770fd887
mfurak <mfurak@configit.com>
2019-02-06 11:24:47 +0100

    Switched to VSCodium, added install script to copy the settings and install extensions

--- vs_codium/extract.sh
+++ vs_codium/extract.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 MY_DIR=./
-VS_PATH=$APPDATA/Code/User;
+#VS_PATH=$APPDATA/Code/User;
+VS_PATH=$APPDATA/VSCodium/User;
 cp -f $VS_PATH/keybindings.json $MY_DIR
 cp -f $VS_PATH/settings.json $MY_DIR
 cp -rf $VS_PATH/snippets/ $MY_DIR
