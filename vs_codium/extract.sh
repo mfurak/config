7bd1074cf1d4d3e3b0d4f55c7c782128602fd140
mfurak <marek.furak@gmail.com>
2022-01-26 19:57:23 +0100

    update vscodium

--- vs_codium/extract.sh
+++ vs_codium/extract.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/bash
 MY_DIR=./
-#VS_PATH=$APPDATA/Code/User;
-VS_PATH=$APPDATA/VSCodium/User;
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    VS_PATH=~/.config/VSCodium/User;
+else
+    VS_PATH=$APPDATA/VSCodium/User;
+fi
+
 cp -f "$VS_PATH"/keybindings.json $MY_DIR
 cp -f "$VS_PATH"/settings.json $MY_DIR
 cp -rf "$VS_PATH"/snippets/ $MY_DIR
-vscodium --list-extensions > extensions_list.txt
+codium --list-extensions > extensions_list.txt
--- vs_codium/install.sh
+++ vs_codium/install.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
-#VS_PATH=$APPDATA/Code/User;
-VS_PATH=$APPDATA/VSCodium/User;
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    VS_PATH=~/.config/VSCodium/User;
+else
+    VS_PATH=$APPDATA/VSCodium/User;
+fi
 cp -rf ./* "$VS_PATH"/
-cat extensions_list.txt | xargs -L 1 vscodium --install-extension
+cat extensions_list.txt | xargs -L 1 codium --install-extension
