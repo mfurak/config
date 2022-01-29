dfca67ee2760cb276b0256a38ebfab8b2cf7beeb
mfurak <marek.furak@gmail.com>
2022-01-29 17:33:28 +0100

    fix vscodium install script

--- vs_codium/install.sh
+++ vs_codium/install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 source ./variables.sh
-cp MY_DIR/keybindings.json "$VS_PATH"
-cp MY_DIR/settings.json "$VS_PATH"
-cp -r MY_DIR/snippets/ "$VS_PATH"
+cp "$MY_DIR"/keybindings.json "$VS_PATH"
+cp "$MY_DIR"/settings.json "$VS_PATH"
+cp -r "$MY_DIR"/snippets/ "$VS_PATH"
 cat "$EXTENSION_LIST" | xargs -L 1 codium --install-extension
 
