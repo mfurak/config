d35df66e81c932fa00d57d9ef1c19feb96b08001
mfurak <marek.furak@gmail.com>
2023-05-10 19:32:32 +0200

    add macos detection

--- vs_codium/variables.sh
+++ vs_codium/variables.sh
@@ -1,10 +1,12 @@
-#! /usr/bin/bash
+#! /bin/bash
 MY_DIR="./"
 INSTALLED_EXTENSIONS=$MY_DIR"extensions_installed.txt"
 UNINSTALLED_EXTENSIONS=$MY_DIR"extensions_uninstalled.txt"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    VS_PATH=~/.config/VSCodium/User;
+    VS_PATH="$HOME/.config/VSCodium/User"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    VS_PATH="$HOME/Library/Application Support/VSCodium/User"
 else
-    VS_PATH=$APPDATA/VSCodium/User;
+    VS_PATH="$APPDATA/VSCodium/User"
 fi
