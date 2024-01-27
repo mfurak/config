485539df06c2fefad48317c014698f2996d26ba8
mfurak <marek.furak@lego.com>
2024-01-27 19:45:56 +0100

    fix vs codium config directory

--- vs-codium/variables.sh
+++ vs-codium/variables.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
-CONFIG_DIRECTORY="~/.config/VSCodium"
 INSTALLED_EXTENSIONS="./extensions_installed.txt"
 UNINSTALLED_EXTENSIONS="./extensions_uninstalled.txt"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    VS_PATH="$HOME/.config/VSCodium/User"
+    CONFIG_DIRECTORY="$HOME/.config/VSCodium/User"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    VS_PATH="$HOME/Library/Application Support/VSCodium/User"
+    CONFIG_DIRECTORY="$HOME/Library/Application Support/VSCodium/User"
 else
-    VS_PATH="$APPDATA/VSCodium/User"
+    CONFIG_DIRECTORY="$APPDATA/VSCodium/User"
 fi
