c387e94b2225a1d76aaaa3644ba5f7133e1d115e
mfurak <marek.furak@lego.com>
2024-01-27 20:09:23 +0100

    extract more variables from vs codium scripts

--- vs-codium/extract.sh
+++ vs-codium/extract.sh
@@ -3,4 +3,4 @@ source ./variables.sh
 cp "$CONFIG_DIRECTORY"/keybindings.json ./
 cp "$CONFIG_DIRECTORY"/settings.json ./
 cp -r "$CONFIG_DIRECTORY"/snippets/ ./
-codium --list-extensions > "./$INSTALLED_EXTENSIONS"
+$EXECUTABLE_NAME --list-extensions > "./$INSTALLED_EXTENSIONS"
--- vs-codium/install.sh
+++ vs-codium/install.sh
@@ -3,5 +3,5 @@ source ./variables.sh
 cp ./keybindings.json "$CONFIG_DIRECTORY"
 cp ./settings.json "$CONFIG_DIRECTORY"
 cp -r ./snippets/ "$CONFIG_DIRECTORY"
-cat "$UNINSTALLED_EXTENSIONS" | xargs --no-run-if-empty -I % codium --uninstall-extension % --force
-cat "$INSTALLED_EXTENSIONS" | xargs --no-run-if-empty -I % codium --install-extension % --force
+cat "$UNINSTALLED_EXTENSIONS" | xargs --no-run-if-empty -I % $EXECUTABLE_NAME --uninstall-extension % --force
+cat "$INSTALLED_EXTENSIONS" | xargs --no-run-if-empty -I % $EXECUTABLE_NAME --install-extension % --force
