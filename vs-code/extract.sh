e200c6161f7d12d9c895f55c80c3adcf5c32ab1b
mfurak <marek.furak@gmail.com>
2024-04-19 16:43:55 +0200

    remove vs-code

--- vs-code/extract.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source ./variables.sh
-cp "$CONFIG_DIRECTORY"/keybindings.json ./
-cp "$CONFIG_DIRECTORY"/settings.json ./
-cp -r "$CONFIG_DIRECTORY"/snippets/ ./
-$EXECUTABLE_NAME --list-extensions > "./$INSTALLED_EXTENSIONS"
