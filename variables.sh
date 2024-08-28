daf529840b32f44163ff0d404af7d0136122f26a
mfurak <marek.furak@lego.com>
2024-08-28 22:53:18 +0200

    update how config files are found

--- variables.sh
+++ variables.sh
@@ -1,1 +1,3 @@
-CONFIG_FILES=$(find . -type f ! -name "install.sh" ! -name "extract.sh" ! -name "variables.sh" ! -name "*.shell_aliases" -maxdepth 1)
+CONFIG_FILES=$(find . -type f ! -name "install.sh" ! -name "extract.sh" ! -name "variables.sh" ! -name "*.shell_aliases" -maxdepth 1 | sed 's|^\./||')
+# set internal field separator to newline to be able to loop over config files
+IFS=$'\n'
