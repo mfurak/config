eec11cdbda4e78ba1a102bb3e6821634d7c14461
mfurak <marek.furak@lego.com>
2026-03-27 11:37:48 +0100

    remove unnecessary file copying functionality

--- setup.mk
+++ setup.mk
@@ -5,26 +5,6 @@ TARGET_ALIASES_DIRECTORY := "$$HOME/.shell_aliases"
 
 LOCAL_CONFIG_DIRECTORY := "config-files"
 
-define copy-files
-	LOCAL_DIRECTORY="$(1)"; \
-	SOURCE_DIRECTORY="$(2)"; \
-	TARGET_DIRECTORY="$(3)"; \
-	FILES=$$(find "$$LOCAL_DIRECTORY" -type f); \
-	IFS=$$'\n'; \
-	for FILE in $$FILES; do \
-		TRIMMED_FILE=$${FILE#*/}; \
-		if [ -n "$(DEBUG)" ]; then \
-			echo "Copying from '$$SOURCE_DIRECTORY/$$TRIMMED_FILE' to '$$TARGET_DIRECTORY/$$TRIMMED_FILE'"; \
-		fi; \
-		mkdir -p "$$TARGET_DIRECTORY"; \
-		if [ -f "$$SOURCE_DIRECTORY/$$TRIMMED_FILE" ]; then \
-			cp "$$SOURCE_DIRECTORY/$$TRIMMED_FILE" "$$TARGET_DIRECTORY/$$TRIMMED_FILE"; \
-		else \
-			echo "$$TRIMMED_FILE doesn't exist at $$SOURCE_DIRECTORY"; \
-		fi; \
-	done
-endef
-
 define symlink-files
 	LOCAL_DIRECTORY="$(1)"; \
 	TARGET_DIRECTORY="$(2)"; \
